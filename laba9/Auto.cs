7a9b6936880a011bd6009b20ed5a2b88770b5c9b
Vladimir <Shifman.1704@mail.ru>
2020-12-23 20:48:44 +0700

    Добавьте файлы проекта.

---
+++ laba9/Auto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba9
+{
+    class Auto
+    {
+        String brand; //марка
+        int power; //цвет
+        int cost;     //стоимость
+       public Auto()
+        {
+            brand = "";
+            power = 0;
+            cost = 0;
+        }
+
+        public Auto(String brand)
+        {
+            this.brand = brand;
+            power = 0;
+            cost = 0;
+        }
+
+        public Auto(String brand, int power, int cost)
+        {
+            this.brand = brand;
+            this.cost = cost;
+            this.power = power;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"{brand}, {power} - {cost}p");
+        }
+
+        public void read()
+        {
+            Console.WriteLine("Марка: ");
+            brand = Console.ReadLine();
+            Console.WriteLine("Мощность: ");
+            power = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Стоимость: ");
+            cost = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
---
+++ laba9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto car1 = new Auto("Ford", 400, 300000);
+            Auto car2 = new Auto("Mazda");
+            Auto car3 = new Auto();          
+            car1.display();
+            car2.display();
+            car3.display();
+
+            Auto[] cars = new Auto[3];
+            for (int i = 0; i < 3; i++)
+                cars[i] = new Auto("Car" + (i + 1).ToString());
+
+            for (int i = 0; i < 3; i++)
+                cars[i].display();
+        }
+    }
+
+}
